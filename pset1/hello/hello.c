b686b3d885799c31bc548ebbe76f4934ade4209b
mattmg83 <mguerville@gmail.com>
2022-07-14 14:50:24 -0500

    Update hello.c
    Fixed typo in a comment

--- pset1/hello/hello.c
+++ pset1/hello/hello.c
@@ -6,6 +6,6 @@ int main(void)
     //Gets the name from the user
     string name = get_string("What's your name?\n");
 
-    //Prints out the salutation plus te name
+    //Prints out the salutation plus the name
     printf("\nHello, %s!\n", name);
 }
